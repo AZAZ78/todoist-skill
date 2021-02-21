a9d4822d2063199ba3435b3189510103e5b34eec
AZAZ78 <alexander.zeh@gmx.de>
2021-02-21 17:54:39 +0100

    Adapted logging, settingsupdate and moved requirements to manifest

--- __init__.py
+++ __init__.py
@@ -6,41 +6,50 @@ import requests
 import json
 from mycroft import MycroftSkill, intent_file_handler
 from mycroft.messagebus.message import Message
-from mycroft.util.log import LOG
 
 class Todoist(MycroftSkill):
     def __init__(self):
-        LOG.debug("Todoist: __init__.")
         MycroftSkill.__init__(self)
+        self.log.debug("__init__")
+        self.token = None
+
+    def initialize(self):
+        self.log.debug("initialize")
+        self.settings_change_callback = self.on_websettings_changed
+        self.setup()
+
+    def on_websettings_changed(self):
+        self.setup()
+
+    def setup(self):
+        # Get API token
+        self.token = self.settings.get('token',"")
+        if not self.token:
+          self.speak('No A P I token provided in the configuration.')
+        return
 
     @intent_file_handler('todoist.intent')
     def handle_todoist(self, message):
-        # Get API token
-        token = self.settings.get('token',"")
-        if token == "":
-          self.speak_dialog("error" , data={"message" : 'No A P I token provided in the configuration.'})
-          return;
-
         # Get item and list name
         item = message.data["item"].capitalize()
         if "listname" in message.data:
           listname = message.data["listname"].capitalize()
         else:
-          self.speak_dialog("error" , data={"message" : 'No list name specified.'})
+          self.speak('No list name specified.')
           return
 
         self.gui["listName"] = listname
         self.gui["itemToAdd"] = item
         self.gui.show_page("display.qml")
-        LOG.debug("Todoist: token: "+token)
-        LOG.debug("Todoist: item: "+item)
-        LOG.debug("Todoist: list: "+listname)
+        self.log.debug("Todoist: token: "+self.token)
+        self.log.debug("Todoist: item: "+item)
+        self.log.debug("Todoist: list: "+listname)
 
-        resp = requests.get('https://api.todoist.com/rest/v1/projects', headers={'Authorization':'Bearer '+token})
-        LOG.debug("JsonResponseCode: "+str(resp.status_code))
+        resp = requests.get('https://api.todoist.com/rest/v1/projects', headers={'Authorization':'Bearer '+self.token})
+        self.log.debug("JsonResponseCode: "+str(resp.status_code))
         if resp.status_code != 200:
-          self.speak_dialog('There was an error calling the todoist A P I.  '+resp.text)
-          return;
+          self.speak('There was an error calling the todoist A P I.  '+resp.text)
+          return
           
         content=resp.json()
         listid = -1
@@ -50,22 +59,19 @@ class Todoist(MycroftSkill):
           LOG.debug("test: "+lowercaseItem+" contains "+lowercaseListname)
           if lowercaseListname in lowercaseItem:
               listid = currentitem['id']
-              LOG.debug("Found: "+str(listid))
+              self.log.debug("Found: "+str(listid))
 
         if listid < 0:
-          self.speak_dialog('Unable to find list, '+listname)
-          return;
+          self.speak('Unable to find list, '+listname)
+          return
         else:
           data = { "comment_count": 0, "completed": "false", "content": item, "project_id": listid}
           resp = requests.post('https://api.todoist.com/rest/v1/tasks', headers={'Content-Type': 'application/json','Authorization':'Bearer '+token}, data=json.dumps(data))
           if resp.status_code != 200:
-            self.speak_dialog('There was an error calling the todoist A P I.  '+resp.text)
+            self.speak('There was an error calling the todoist A P I.  '+resp.text)
             return;
-          LOG.debug("POST JsonResponseCode: "+str(resp.status_code))
+          self.log.debug("POST JsonResponseCode: "+str(resp.status_code))
           self.speak_dialog("todoist" , data={"item" : item, "listname" : listname})
-
-    def initialize(self):
-        LOG.debug("Todoist: initialize.")
 
 def create_skill():
     return Todoist()
